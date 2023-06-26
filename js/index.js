f2b5d50614ea018792c8470f11679bc91315bd33
sexybell <nguyenhaid98@yahoo,cin>
2023-06-26 21:55:53 +0700

    first

---
+++ js/index.js
@@ -0,0 +1,19 @@
+new Splide( '.splide' ).mount();
+
+var splide = new Splide('.splide', {
+    type: 'loop',
+    perPage: 4,
+    perMove: 1
+})
+
+splide.mount();
+
+new Splide( '.highlight' ).mount();
+
+var splide = new Splide('.highlight', {
+    type: 'loop',
+    perPage: 4,
+    perMove: 1
+})
+
+splide.mount();
